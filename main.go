39f245e48882f3fa0111c447fb23d274b8bdb3e8
jstnf <justin@justinf.dev>
2024-04-29 00:05:03 -0500

    feat: Initialize postgres store and provide to router

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/jstnf/the-button-server/api"
+	"github.com/jstnf/the-button-server/data"
 	"log"
 	"os"
 )
@@ -12,7 +13,16 @@ func main() {
 		port = "3001"
 	}
 
-	router := api.NewAPIServer(":"+port, nil)
+	store, err := data.NewPostgresStorage(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Failed to create storage: %v", err)
+	}
+	defer store.Close()
+	if err := store.Init(); err != nil {
+		log.Fatalf("Failed to initialize storage: %v", err)
+	}
+
+	router := api.NewAPIServer(":"+port, store)
 
 	log.Printf("Server is running on port %s", port)
 	log.Fatal(router.Run())
