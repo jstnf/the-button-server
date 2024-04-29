a1b20b24a7be27ec839a31a9fd533997c68786ff
jstnf <justin@justinf.dev>
2024-04-28 23:34:42 -0500

    feat: Create button errors

---
+++ data/errors.go
@@ -0,0 +1,5 @@
+package data
+
+const ErrorUserUnknown = "Unknown user. Are you using the correct link to the button?"
+const ErrorPressedTooSoon = "You must wait 15s between button presses."
+const ErrorPressedTwice = "You can't press the button twice in a row. Please wait for someone else to press the button."
