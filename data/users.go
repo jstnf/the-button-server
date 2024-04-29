1b8d8f8de33bba99c5f52f1fdd812c89377f048b
jstnf <justin@justinf.dev>
2024-04-29 01:05:01 -0500

    feat: Local user storage

---
+++ data/users.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type UserStorage interface {
+	GetUserById(id string) (*User, error)
+}
+
+type LocalUserStorage struct {
+	users map[string]*User
+}
+
+func NewLocalUserStorage() *LocalUserStorage {
+	return &LocalUserStorage{
+		users: make(map[string]*User),
+	}
+}
+
+func (s *LocalUserStorage) Init() error {
+	jsonFile, err := os.Open("users.json")
+	if err != nil {
+		return err
+	}
+	defer func(jsonFile *os.File) {
+		err := jsonFile.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(jsonFile)
+
+	byteValue, _ := io.ReadAll(jsonFile)
+	var users []User
+	if err := json.Unmarshal(byteValue, &users); err != nil {
+		return err
+	}
+	for _, user := range users {
+		s.users[user.UserId] = &user
+	}
+	return nil
+}
+
+func (s *LocalUserStorage) GetUserById(id string) (*User, error) {
+	return s.users[id], nil
+}
